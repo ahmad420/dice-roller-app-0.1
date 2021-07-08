672ba0ff3ffd0f953d943fc6707f6777797eb3a3
Ahmad Af <ahmdaf420@gmail.com>
2021-07-08 04:30:14 +0300

    added dice roll image

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.dicerollerapp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,22 +13,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton : Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
-            val resultTextView: TextView = findViewById(R.id.textView)
                 rollDice()
+
             }
         }
 
     private fun rollDice() {
         val dice = Dice(6)
-        val diceRoll = dice.roll()
+        val diceResult = dice.roll()
+
         val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        resultTextView.text = diceResult.toString()
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        val drawableResource = when(diceResult){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceResult.toString()
+
+
+        val luckyNum = 4
+        if (diceResult == luckyNum){
+            val toast = Toast.makeText(this, "lucky number, you win!!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
     }
 }
 
-class Dice(val numSides: Int) {
+class Dice(private val numSides: Int) {
 
     fun roll(): Int {
         return (1..numSides).random()
